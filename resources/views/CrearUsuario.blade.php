d5ddcf8ca1b2a527f2f4a0d72b42c556d20da4e5
katiuskizzz <munozkatalinavalderrama@gmail.com>
2023-11-21 07:57:59 -0500

    tailwind

--- resources/views/CrearUsuario.blade.php
+++ resources/views/CrearUsuario.blade.php
@@ -3,22 +3,22 @@
 @section("tituloPagina", "Crear un nuevo usuario")
 
 @section('contenido')
-    <div class=" w-full text-3xl">
-        <h1>Registrar usuario</h1>
+    <div class="w-full text-xl ">
+        <h1 class="mb-10 text-3xl font-bold">Registrar usuario</h1>
         <form action="{{route('Users.store')}}" method="POST">
             @csrf
-            <div class=" p-4">
+            <div class="">
 
-                <label class=" ">Nombre</label><br>
-                <input  class="border border-black " type="text" name="name" placeholder="Ingrese su nombre"><br>
+                <label class="">Nombre</label><br>
+                <input  class="m-4 border border-black " type="text" name="name" placeholder="Ingrese su nombre"><br>
                 <label>Identificacion</label><br>
-                <input class="border border-black" type="text" name="identification" placeholder="Ingrese su identificacion"><br>
+                <input class="m-4 border border-black" type="text" name="identification" placeholder="Ingrese su identificacion"><br>
                 <label>Telefono</label><br>
-                <input  class="border border-black "type="text" name="phone" placeholder="Ingrese su telefono"><br>
+                <input  class="m-4 border border-black "type="text" name="phone" placeholder="Ingrese su telefono"><br>
                 <label>Direccion</label><br>
-                <input class="border border-black" type="text" name="address" placeholder="Ingrese su direccion"> <br><br>
-                <button class="bg-blue-400 w-44 h-10 text-white text-center">Agregar</button>
-                <a href="{{route("users.index")}}"  class="bg-blue-400 w-44 h-10 text-white text-center">Regresar</a>
+                <input class="m-4 border border-black " type="text" name="address" placeholder="Ingrese su direccion"> <br><br>
+                <button class="h-10 text-white bg-blue-400 m-4text-center w-44">Agregar</button>
+                <a href="{{route("users.index")}}"  class="h-10 text-center text-white bg-blue-400 w-44">Regresar</a>
             </div>
         </form>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,9 @@
 @extends('layout/plantilla')
 
-    <div class=" flex w-screen h-20 text-3xl font-bold  justify-center items-center bg-gray-400">
+    <div class="flex items-center justify-center w-screen h-20 text-3xl font-bold bg-gray-400 ">
         <h1 class="">CRUD</h1>
     </div>
-    <h2 class=" flex   justify-center items-center text-3xl font-bold ">Listado de usuarios</h2>
+    <h2 class="flex items-center justify-center text-3xl font-bold ">Listado de usuarios</h2>
     @if($mensaje = Session::get('success'))
     <div>
         {{$mensaje}}
@@ -11,46 +11,47 @@
     @endif
     <div class="h-20">
         <p class="">
-            <a href="{{route ("users.create")}}" class="bg-blue-700 text-white text-3xl"><span><i class="fa-solid fa-user-plus"></i></span>Crear usuario</a>
+            <a href="{{route ('users.create')}}" class="text-3xl text-white bg-blue-700"><span><i class="fa-solid fa-user-plus"></i></span>Crear usuario</a>
         </p>
     </div>
-    <div class="flex w-screen h-30  justify-center items-center">
-            <table class="border-collapse border border-gray-300">
+    <div class="flex items-center justify-center w-screen h-30">
+            <table class="border border-collapse border-gray-300">
                 <thead class="gap-x-1">
-                    <th class="border border-gray-300 p-2"> Nombre</th>
-                    <th class="border border-gray-300 p-2">Identificacion</th>
-                    <th class="border border-gray-300 p-2">Celular</th>
-                    <th class="border border-gray-300 p-2">Direccion </th>
-                    <th class="border border-gray-300 p-2">Editar</th>
-                    <th class="border border-gray-300 p-2">Eliminar</th>
-                    <th class="border border-gray-300 p-2">PDF</th>
+                    <th class="p-2 border border-gray-300"> Nombre</th>
+                    <th class="p-2 border border-gray-300">Identificacion</th>
+                    <th class="p-2 border border-gray-300">Celular</th>
+                    <th class="p-2 border border-gray-300">Direccion </th>
+                    <th class="p-2 border border-gray-300">Editar</th>
+                    <th class="p-2 border border-gray-300">Eliminar</th>
+                    <th class="p-2 border border-gray-300">PDF</th>
 
                 </thead>
-                <tbody>
+                <tbody class="text-center">
                     @foreach ($datos as $info)
                         <tr>
-                            <td class="border border-gray-300 p-2">{{$info ->name }}</td>
-                            <td class="border border-gray-300 p-2">{{$info ->identification}}</td>
-                            <td class="border border-gray-300 p-2">{{$info ->phone}}</td>
-                            <td class="border border-gray-300 p-2">{{$info ->address}}</td>
-                            <td class="border border-gray-300 p-2">
+                            <td class="p-2 border border-gray-300">{{$info ->name }}</td>
+                            <td class="p-2 border border-gray-300">{{$info ->identification}}</td>
+                            <td class="p-2 border border-gray-300">{{$info ->phone}}</td>
+                            <td class="p-2 border border-gray-300">{{$info ->address}}</td>
+                            <td class="w-10 p-2 border border-gray-300">
                                 <form action="{{route('Users.edit', $info->id)}}" method="GET">
-                                    <button class="bg-blue-400 w-44 h-10 text-white text-center">
+                                    <button class="w-10 h-10 text-center text-blue-600 w-44">
                                         <span><i class="fa-solid fa-pen"></i></span>
                                     </button>
                                 </form>
                             </td>
 
-                            <td class="border border-gray-300 p-2">
-                                <form action="{{route('Users.show', $info->id)}}" method="GET">
-                                    <button class="bg-blue-400 w-44 h-10 text-white text-center">
-                                        <span><i class="fa-solid fa-delete-left"></i></span>
+                            <td class="w-4 p-2 border border-gray-300">
+                                <form action="{{ route('Users.show', $info->id) }}" method="GET">
+                                    <button class="w-10 h-10 text-center text-blue-600">
+                                        <span><i class="text-sm fa-solid fa-delete-left"></i></span>
                                     </button>
                                 </form>
                             </td>
-                            <td class="border border-gray-300 p-2">
+
+                            <td class="w-10 p-2 border border-gray-300">
                                 <form action="{{route('Users.report', $info->id)}}" method="GET">
-                                    <button class="bg-blue-400 w-44 h-10 text-white text-center">
+                                    <button class="h-10 text-center text-white bg-blue-400 rounded w-36">
                                         <span>Descargar</span>
                                     </button>
                                 </form>
